af018729e0dd5d7422fb758f11555f90b7e23010
Vicafe04 <vinicius51885189@gmail.com>
2023-05-22 11:35:03 -0300

    22/05

--- FrontEnd/yourpet/script.js
+++ FrontEnd/yourpet/script.js
@@ -1,24 +1,25 @@
+const lista = document.querySelector('.list')
+const itemlista = document.querySelector('.item-list')
+const userID = localStorage.getItem('userID')
+
 function load() {
     const options = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     };
-    fetch("http://localhost:3000/Hotel",options)
+    fetch("http://localhost:3000/Pet",options)
         .then(resp => { return resp.json() })
         .then(frota => {
+            console.log(frota)
             frota.forEach(e => {
                 console.log(e)
                 let item = itemlista.cloneNode(true);
                 item.classList.remove("model");
-                item.querySelector("#id").innerHTML = 'id do pet: ' + e.pets[0].id;
-                item.querySelector("#nome").innerHTML = 'nome do pet: ' + e.pets[0].nome;
-                item.querySelector("#porte").innerHTML = 'porte: ' + e.pets[0].porte;
-                item.querySelector("#descricao").innerHTML = 'descrição: ' + e.pets[0].descricao;
-                item.querySelector("#hotelId").innerHTML = 'id da Reserva: ' + e.pets[0].hotelId;
-                var getDate = e.horaPedido.split('T', 1);
-                item.querySelector("#horaReserva").innerHTML = `<input disabled type="date" value="${getDate}">`
-                item.querySelector("#delBTN").setAttribute("onclick", `del('${e.pets[0].id}')`);
-             
+                item.querySelector("#id").innerHTML = 'Id: ' + e.id;
+                item.querySelector("#nome").innerHTML = 'Nome: ' + e.nome;
+                item.querySelector("#porte").innerHTML = 'Porte: ' + e.porte;
+                item.querySelector("#descricao").innerHTML = 'Descrição: ' + e.descricao;
+                
                 lista.appendChild(item);
             })
         })
