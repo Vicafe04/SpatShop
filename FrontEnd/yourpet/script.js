f050d7346e24722995728b9c5132a61284c39fc3
Vicafe04 <vinicius51885189@gmail.com>
2023-05-24 11:22:50 -0300

    24/05

--- FrontEnd/yourpet/script.js
+++ FrontEnd/yourpet/script.js
@@ -10,15 +10,13 @@ function load() {
     fetch("http://localhost:3000/Pet",options)
         .then(resp => { return resp.json() })
         .then(frota => {
-            console.log(frota)
             frota.forEach(e => {
-                console.log(e)
                 let item = itemlista.cloneNode(true);
                 item.classList.remove("model");
-                item.querySelector("#id").innerHTML = 'Id: ' + e.id;
-                item.querySelector("#nome").innerHTML = 'Nome: ' + e.nome;
-                item.querySelector("#porte").innerHTML = 'Porte: ' + e.porte;
-                item.querySelector("#descricao").innerHTML = 'Descrição: ' + e.descricao;
+                item.querySelector("#id").innerHTML = 'Id:<br/>' + e.id;
+                item.querySelector("#nome").innerHTML = 'Nome:<br/>' + e.nome;
+                item.querySelector("#porte").innerHTML = 'Porte:<br/>' + e.porte;
+                item.querySelector("#descricao").innerHTML = '       Descrição:<br/>' + e.descricao;
                 
                 lista.appendChild(item);
             })
