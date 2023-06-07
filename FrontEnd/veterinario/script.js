c8227ee20f15998dfd2162910111344bd6058f6e
MCarlosEduardoS <106968731+MCarlosEduardoS@users.noreply.github.com>
2023-06-07 10:38:17 -0300

    Add files via upload

--- FrontEnd/veterinario/script.js
+++ FrontEnd/veterinario/script.js
@@ -1,100 +1,84 @@
-const abrirModal = document.getElementById('abrirModal');
-const modal = document.getElementById('meuModal');
-const fechar = document.getElementsByClassName('fechar')[0];
-const cadastrarServicoBtn = document.querySelector('.btn');
-const lista = document.getElementById('lista-dados');
+const lista = document.querySelector('.list')
+const itemlista = document.querySelector('.item-list')
+const opcList = document.querySelector('.opcList')
+const inpId = document.querySelector('#inpId')
+const descricao = document.querySelector('#ob')
+const userID = localStorage.getItem('userID')
 
-abrirModal.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-fechar.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-});
-
-cadastrarServicoBtn.addEventListener('click', async () => {
-  const idPet = document.querySelector('input[type="text"]').value;
-  const tipoServico = document.querySelector('#raca').value;
-
-  try {
-    const response = await fetch('http://localhost:3000/Servicos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ idPet, tipoServico })
-    });
-
-    alert('Serviço cadastrado com sucesso!');
-
-    modal.style.display = 'none';
-
-  } catch (error) {
-    console.error(error);
-    alert('Erro ao cadastrar serviço');
-  }
-});
-
-function exibirDados(dados) {
-  const lista = document.getElementById('lista-dados');
-
-  dados.forEach((item) => {
-    const li = document.createElement('li');
-    li.textContent = item.nome; 
-
-    lista.appendChild(li);
-  });
+function load() {
+    fetch("http://localhost:3000/Servicos")
+        .then(resp => { return resp.json() })
+        .then(reserva => {
+            reserva.forEach(e => {
+                let item = itemlista.cloneNode(true);
+                item.classList.remove("model");
+                item.querySelector("#servico").innerHTML = 'nome do serviço: ' + e.servicTrab;    
+                item.querySelector("#profissional").innerHTML = 'nome do funcionario ' + e.nomeEmpreg;
+                item.querySelector("#descricao").innerHTML = 'servico info: '+ e.descricao;
+                item.querySelector("#servicoId").innerHTML = 'id do servico: ' + e.id;
+                lista.appendChild(item);
+            })
+        })
 }
-
-fetch('http://localhost:3000/Servicos') 
-  .then(response => response.json())
-  .then(data => exibirDados(data))
-  .catch(error => console.error(error));
-
-  function exibirDados(dados) {
-    const lista = document.getElementById('lista-dados');
-  
-    dados.forEach((item) => {
-      const li = document.createElement('li');
-      li.textContent = item.nome; 
-  
-      const deleteBtn = document.createElement('button');
-      deleteBtn.textContent = 'Deletar';
-      deleteBtn.addEventListener('click', () => {
-        deletarServico(item.id); 
-      });
-  
-      li.appendChild(deleteBtn);
-      lista.appendChild(li);
-    });
-  }
-  
-
-  function deletarServico(id) {
-    
-    fetch(`http://localhost:3000/Servicos/${id}`, {
-      method: 'DELETE'
-    })
-      .then(response => {
-        if (response.ok) {
-          
-          const listItem = document.querySelector(`li[data-id="${id}"]`);
-          listItem.remove();
-        } else {
-          console.error('Erro ao deletar o serviço.');
-        }
-      })
-      .catch(error => console.error(error));
-  }
-  
-  
-  fetch('http://localhost:3000/Servicos') 
-    .then(response => response.json())
-    .then(data => exibirDados(data))
-    .catch(error => console.error(error));
+function loadServ() {
+    fetch("http://localhost:3000/Servicos")
+        .then(resp => { return resp.json() })
+        .then(Pet => {
+            Pet.forEach(e => {
+                if (e.userId == userID) {
+                    const opc = document.createElement("option");
+                    opc.innerHTML = e.servicTrab
+                    opc.innerHTML = e.nomeEmpreg
+                    opc.innerHTML = e.descricao
+                    opc.value = e.id
+                    opcList.appendChild(opc);
+                }
+            })
+        })
+}
+function del(id) {
+    if (confirm("Confirma a exclusão do serviço")) {
+        const options = {
+            method: 'DELETE'
+        };
+        fetch(`http://localhost:3000/Servicos/${id}`, options)
+            .then(resp => resp.status)
+            .then(resp => {
+                if (resp == 200) {
+                    window.location.reload();
+                }
+            })
+            .catch(err => alert("Erro ao enviar dados, Erro:" + err));
+    }
+}
+function reservar() {
+    if(descricao.value == null)
+    descricao.value = "";
+    let dados = {
+        descricao: descricao.value,
+        servico: Number(inpId.value)
+    }
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+    };
+    fetch("http://localhost:3000/Servicos", options)
+        .then(response => {
+            return response.json();
+        })
+        .then(resp => {
+            console.log(resp)
+            if (resp != null) {
+                alert("Serviço cadastrado)")
+                window.location.reload()
+            } else {
+                
+                console.log(resp)
+            }
+        })
+        .catch(err => console.error(err));
+}
+opcList.addEventListener("change", () => {
+    document.querySelector("#inpId").value = opcList.value
+})
